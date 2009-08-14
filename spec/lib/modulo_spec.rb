35aec544696c752538be40dd5628a6964008ce5f
Lonnon Foster <lonnon.foster@gmail.com>
2009-08-14 15:09:45 -0700

    Added by="title" to specs
    Despite the fact that this worked fine a couple days ago,
    <r:children:each> is now iterating through the test data out-of-order,
    resulting in pages(:parent) spitting out "Child Child3 Child 2" instead
    of the expected order. Can work around it by adding the by option to
    <r:children:each>, but it concerns me that this passed just fine a
    couple days ago, and I haven't changed anything in the meantime.
    
    The code seems to work just fine with the live data I've got, which
    isn't calling <r:children:each> with a by attribute. I'm really at a
    loss here.

--- spec/lib/modulo_spec.rb
+++ spec/lib/modulo_spec.rb
@@ -5,17 +5,17 @@ describe 'Modulo' do
   
   describe "<r:if_even>" do
     it "should print the tag's contents if we're in an even child" do
-      markup = '<r:children:each><r:if_even><r:title /></r:if_even></r:children:each>'
+      markup = '<r:children:each by="title"><r:if_even><r:title /></r:if_even></r:children:each>'
       
       expected = "Child 2"
-      
+
       pages(:parent).should render(markup).as(expected)
     end
   end
   
   describe "<r:unless_even>" do
     it "should not print the tag's contents if we're in an even child" do
-      markup = '<r:children:each><r:unless_even><r:title /></r:unless_even></r:children:each>'
+      markup = '<r:children:each by="title"><r:unless_even><r:title /></r:unless_even></r:children:each>'
     
       expected = "ChildChild 3"
       
@@ -25,7 +25,7 @@ describe 'Modulo' do
   
   describe "<r:if_odd>" do
     it "should print the tag's contents if we're in an odd child" do
-      markup = '<r:children:each><r:if_odd><r:title /></r:if_odd></r:children:each>'
+      markup = '<r:children:each by="title"><r:if_odd><r:title /></r:if_odd></r:children:each>'
       
       expected = "ChildChild 3"
       
@@ -35,7 +35,7 @@ describe 'Modulo' do
 
   describe "<r:unless_odd>" do
     it "should not print the tag's contents if we're in an odd child" do
-      markup = '<r:children:each><r:unless_odd><r:title /></r:unless_odd></r:children:each>'
+      markup = '<r:children:each by="title"><r:unless_odd><r:title /></r:unless_odd></r:children:each>'
       
       expected = "Child 2"
       
